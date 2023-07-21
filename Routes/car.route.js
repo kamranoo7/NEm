e0fbcd0ab8ae3d87fd848d77b7b727a6eba510cf
MD Kamran khan <mohammadkkhan78@gmail.com>
2023-07-21 22:52:56 +0530

    ss

--- Routes/car.route.js
+++ Routes/car.route.js
@@ -66,7 +66,7 @@ res.send(data)
 //Sort_Price_Descending
 carRouter.get("/descending",async(req,res)=>{
     try{
-        let data=await CarModel.find().sort({price:-1})
+        let data=await CarModel.find({userID:req.body.userID}).sort({price:-1})
         res.send(data)
     }catch(err){
         res.status(400).send({"msg":err.message}) 
