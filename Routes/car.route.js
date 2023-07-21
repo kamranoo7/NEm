45c089366eabbff8ef042f09db5982825dd33a14
MD Kamran khan <mohammadkkhan78@gmail.com>
2023-07-21 21:57:58 +0530

    ss

--- Routes/car.route.js
+++ Routes/car.route.js
@@ -31,9 +31,9 @@ carRouter.get("/",async(req,res)=>{
 carRouter.patch("/update/:postID",async(req,res)=>{
     
     let {postID}=req.params
-    let payload=req.body
+    let {image,title,color,price,mileage}=req.body
     try{
-        await CarModel.findByIdAndUpdate({_id:postID},payload)
+        await CarModel.findByIdAndUpdate({_id:postID},   let {image,title,color,price,mileage})
         res.status(200).json({msg:"Car has been updated"})
     }catch(err){
         res.status(400).json({error:err.message})
